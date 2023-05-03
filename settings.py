7d0a8c2e15077c0d5c6daa4c31e738bf0d27793e
ajs02 <antonia.jaimeson-sh@bison.howard.edu>
2023-05-03 18:39:15 -0400

    Initial commit

---
+++ settings.py
@@ -0,0 +1,7 @@
+#settings needed by the other files
+SEARCH_KEY = "AIzaSyBaq77wY26I6Ea36o1Az5m3mWeFCh6pr8w"
+SEARCH_ID = "90279d6ac687f4086"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
